80ee3f3cec5a3115ae61f3d9b285453ef1119247
JesterFigueiredo <jesterfigueiredo@gmail.com>
2022-04-22 14:30:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+
+app.set('view engine','ejs');
+app.use(expressLayouts);
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true)
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+})
+
+app.listen(process.env.PORT);
+//process.env.PORT
